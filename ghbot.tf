5f6e01b28de45cbf315fabb493e8ed47e6d3ab6c
Vinay <Developer12@virtuoustech.com>
2024-08-24 10:13:22 +0530

    das

--- ghbot.tf
+++ ghbot.tf
@@ -4,38 +4,57 @@ data "github_repository" "repo" {
 }
 
 data "github_repositories" "example" {
-  query = join("", ["owner:",var.owner," topic:",var.topic,])
+  query = join("", ["owner:",var.owner," topic:",var.topic])
   include_repo_id = true
 }
 
 output "gh-repo-example"{
   value = data.github_repositories.example.names
 }
-resource "github_repository_environment" "repo_environment" {
-  provider      = github.ghorg
-  environment  = var.environment_name
-  repository   = data.github_repository.repo.id
-  reviewers {
-    users = var.environment_name == "production" ? [data.github_user.current.id] : null
-  }
-}
 
-resource "github_actions_environment_variable" "example_variable" {
+resource "github_actions_variable" "example_variable" {
+  for_each      = { for repo in data.github_repositories.example.names : repo => { repo_name = repo }}
   provider      = github.ghorg
-  repository       = data.github_repository.repo.name
-  environment      = github_repository_environment.repo_environment.environment
+  repository       = each.value.repo_name
   variable_name    = "example_variable_name"
   value            = "example_variable_value"
 }
 
-resource "github_actions_variable" "umami_client_id_build_ghav" {
-  provider      = github.ghorg
-  depends_on    = [azurerm_user_assigned_identity.uami_identity_build]
-  for_each      = var.gh_repos
-  repository    = each.value.repo_name
-  variable_name = "ENV_AZURE_CLIENT_ID_BUILD"
-  value         = azurerm_user_assigned_identity.uami_identity_build[each.key].client_id //principal_id
-}
+# resource "github_actions_variable" "umami_client_id_build_ghav" {
+#   provider      = github.ghorg
+#   depends_on    = [azurerm_user_assigned_identity.uami_identity_build]
+#   for_each      = var.gh_repos
+#   repository    = each.value.repo_name
+#   variable_name = "ENV_AZURE_CLIENT_ID_BUILD"
+#   value         = azurerm_user_assigned_identity.uami_identity_build[each.key].client_id //principal_id
+# }
+
+# resource "github_repository_environment" "repo_environment" {
+#   provider      = github.ghorg
+#   environment  = var.environment_name
+#   repository   = data.github_repository.repo.id
+#   reviewers {
+#     users = var.environment_name == "production" ? [data.github_user.current.id] : null
+#   }
+# }
+
+# resource "github_actions_environment_variable" "example_variable" {
+#   provider      = github.ghorg
+#   repository       = data.github_repository.repo.name
+#   environment      = github_repository_environment.repo_environment.environment
+#   variable_name    = "example_variable_name"
+#   value            = "example_variable_value"
+# }
+
+
+# resource "github_actions_variable" "umami_client_id_build_ghav" {
+#   provider      = github.ghorg
+#   depends_on    = [azurerm_user_assigned_identity.uami_identity_build]
+#   for_each      = var.gh_repos
+#   repository    = each.value.repo_name
+#   variable_name = "ENV_AZURE_CLIENT_ID_BUILD"
+#   value         = azurerm_user_assigned_identity.uami_identity_build[each.key].client_id //principal_id
+# }
 
 # # STORAGE_CONNECTION
 # resource "github_actions_environment_secret" "STORAGE_CONNECTION" {
--- terraform.tfvars
+++ terraform.tfvars
@@ -5,10 +5,11 @@ gh_repos = {
     gitignore_template = "Terraform"
     has_wiki           = true
     
+  },
+  "vkit2" = {
+    gh_org = "vinayprakash893"
+    repo_name          = "v-welcome-kit2"
+    gitignore_template = "Terraform"
+    has_wiki           = true
   }
-  # "vkit2" = {
-  #   repo_name          = "v-welcome-kit"
-  #   gitignore_template = "Terraform"
-  #   has_wiki           = true
-  # }
 }
